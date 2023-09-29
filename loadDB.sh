f0bb2956b6861fc7b78a5346eea977ba630d297f
Nath <nathinabob@gmail.com>
2023-09-29 14:38:23 +0100

    update loadDB to current container names

--- loadDB.sh
+++ loadDB.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 echo "Importing mysql database"
-docker exec -i zume-training-db-1 mysql -psomewordpress -uroot wp_zume5 < ./tables/wp_zume5.sql
+docker exec -i zume-training-fresh-db-1 mysql -psomewordpress -uroot wp_zume5 < ./tables/mysql.sql
 echo "replacing zume5.training with zume5.test in database"
-docker exec -i zume-training-wp-cli-1 wp search-replace 'zume5.training' 'zume5.test' --precise --recurse-objects --all-tables
-docker exec -i zume-training-wp-cli-1 wp search-replace 'zume5.wpengine.com' 'zume5.test' --precise --recurse-objects --all-tables
-docker exec -i zume-training-wp-cli-1 wp search-replace 'https://' 'http://' --precise --recurse-objects --all-tables
+docker exec -i zume-training-fresh-wp-cli-1 wp search-replace 'zume5.training' 'zume5.test' --precise --recurse-objects --all-tables
+docker exec -i zume-training-fresh-wp-cli-1 wp search-replace 'zume5.wpengine.com' 'zume5.test' --precise --recurse-objects --all-tables
+docker exec -i zume-training-fresh-wp-cli-1 wp search-replace 'https://' 'http://' --precise --recurse-objects --all-tables
